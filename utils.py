58602615ebd3929605db532d5493f67e1e01b239
TELLYHUBCLOUD <147921265+TELLYHUBCLOUD@users.noreply.github.com>
2023-11-25 11:02:05 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -137,10 +137,10 @@ def list_to_str(k):
     else:
         return ' '.join(f'{elem}, ' for elem in k)
 
-__repo__ = "https://github.com/MrMKN/PROFESSOR-BOT"
-__version__ = "PROFESSOR-BOT ᴠ4.5.0"
+__repo__ = "https://github.com/TELLYHUBCLOUD/its-OK"
+__version__ = "itsOK-BOT ᴠ4.5.0"
 __license__ = "GNU GENERAL PUBLIC LICENSE V2"
-__copyright__ = "Copyright (C) 2023-present MrMKN <https://github.com/MrMKN>"
+__copyright__ = "Copyright (C) 2023-present MrHUB <https://github.com/tellycloud>"
 
 async def search_gagala(text):
     usr_agent = {
